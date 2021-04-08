50ee44544af90a610c51918d2c8bac68ac496d87
JohnnyGomezzz <seldesign@mail.ru>
2021-04-08 16:38:26 +0300

    start

---
+++ server/src/main/java/ru/johnnygomezzz/Server.java
@@ -0,0 +1,35 @@
+package ru.johnnygomezzz;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket server;
+    private Socket socket;
+    private final int PORT = 8189;
+
+    public Server() {
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("server started!");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("client connected " + socket.getRemoteSocketAddress());
+                new ClientHandler(this, socket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            SQLHandler.disconnect();
+            System.out.println("server closed");
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
